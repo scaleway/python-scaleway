e597e0d6a586571f267c7330a97949573f9a9c35
Romain Gay <rgay@ocs.online.net>
2013-08-22 17:25:57 +0200

    added servers test

---
+++ poc/servers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import time
+
+from client import ApiClient
+from pprint import pprint
+
+from random import randrange
+
+client = ApiClient('access_key', 'secret_key')
+
+raw_input('====> creating serv [press a key]')
+server_id = client.request('/servers/', method='POST', blocking=True)['server_id']
+print('server_id', server_id)
+
+raw_input('====> getting serv [press a key]')
+server = client.request('/servers/%s' % server_id)
+print('server', server)
+
+#TEST THIS 
+raw_input('====> [UNFINISHED]switching on server (and thus creating session) [press a key]')
+client.request('/servers/%s/action/' % server_id, method='POST', data={'action':'powerOn'})
+
+raw_input('====> getting serv [press a key]')
+server = client.request('/servers/%s' % server_id)
+print('server', server)
+
+raw_input('====> getting session [press a key]')
+session = client.request('/servers/%s/sessions/%s' % (server_id, server.session))
+print('session', session)
