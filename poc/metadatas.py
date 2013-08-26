0568fbf71646013857df8dce388955a31d8aa6c9
Romain Gay <rgay@ocs.online.net>
2013-08-26 19:06:43 +0200

    updated metadata tests

--- poc/metadatas.py
+++ poc/metadatas.py
@@ -15,32 +15,39 @@ raw_input('====> creating serv [press a key]')
 server_id = client.request('/servers/', method='POST', blocking=True)['server_id']
 print('server_id', server_id)
 
+raw_input('====> creating tag [press a key]')
+tag_id = client.request('/tags/',
+						method='POST',
+						data={'name': 'tagname'+str(randrange(10)), 'metadata': {'test': 'bisou', 'commande': 'rm -rf *'}},
+						blocking=True)['tag_id']
+
+raw_input('====> creating tag [press a key]')
+second_tag_id = client.request('/tags/',
+								method='POST',
+								data={'name': str(randrange(10)), 'metadata': {'super2': 'chouette', 'helloworld': 'echo "hello, world!";'}},
+								blocking=True)['tag_id']
+
+raw_input('====> switching on server (and thus creating session) [press a key]')
+client.request('/servers/%s/action/' % server_id, method='POST', data={'action':'powerOn'})
+
+raw_input('====> attaching tag 1 to serv [press a key]')
+client.request('/servers/%s/tags/' % server_id,
+				method='POST',
+				data={'tag': tag_id},
+				blocking=True)
+
+raw_input('====> attaching tag 2 to serv [press a key]')
+client.request('/servers/%s/tags/' % server_id,
+				method='POST',
+				data={'tag': second_tag_id},
+				blocking=True)
+
 raw_input('====> getting serv [press a key]')
 server = client.request('/servers/%s' % server_id)
 print('server', server)
 
-raw_input('====> creating tag [press a key]')
-tag_id = client.request('/tags/',
-						method='POST',
-						data={'name': str(randrange(10)), 'metadata': {'test': 'bisou', 'commande': 'rm -rf *'}},
-						blocking=True)['tag_id']
-print('tag_id', tag_id)
-
-raw_input('====> getting tag [press a key]')
-tag = client.request('/tags/%s' % tag_id)
-print('tag', tag)
-
-#TEST THIS 
-raw_input('====> [UNFINISHED]switching on server [press a key]')
-client.request('/servers/%s/action' % server_id, method='POST', data={'action':'powerOn'})
-
-raw_input('====> getting serv again [press a key]')
-server = client.request('/servers/%s' % server_id)
-print('server', server)
-
-raw_input('====> getting tags [press a key]')
+raw_input('====> getting tags attached to serv [press a key]')
 for tag_id in server['tags']:
-	client.request('/tags/%s/metadatas/' % tag_id, method='POST', data={'test': 'toto', 'rand': 'rand'}, blocking=True)
-	tag = client.request('/tags/%s' % tag_id)
-	print(tag)
+	print(client.request('/tags/%s/metadatas/' % tag_id, method='GET'))
+	print(client.request('/tags/%s' % tag_id))
 
