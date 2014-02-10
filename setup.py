21e023a4d04983e49c43dde572a4636f2766cddc
Julien Castets <jcastets@ocs.online.net>
2014-02-10 15:44:31 +0100

    Create python module + setup.py

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import imp
+import os
+
+from setuptools import setup
+
+
+MODULE_NAME = 'ocs_sdk'
+MODULE = imp.load_module(MODULE_NAME, *imp.find_module(MODULE_NAME))
+
+
+def get_long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    return open(readme).read()
+
+
+setup(
+    name='ocs-sdk',
+    version=MODULE.__version__,
+    description='OCS APIs client',
+    long_description=get_long_description(),
+
+    author='OCS',
+    author_email='contact@ocs.online.net',
+    url='http://online.net',
+
+    install_requires=[
+
+    ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Topic :: System :: Distributed Computing',
+    ],
+
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
