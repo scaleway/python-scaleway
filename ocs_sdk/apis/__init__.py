a1973bb7c1393839e71e452c8fc1f889826f185c
Kevin Deldycke <kdeldycke@ocs.online.net>
2014-12-11 14:22:20 +0100

    Add missing copyright headers.

--- ocs_sdk/apis/__init__.py
+++ ocs_sdk/apis/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013-2014 Online SAS and Contributors. All Rights Reserved.
+#                         Julien Castets <jcastets@ocs.online.net>
+#                         Kevin Deldycke <kdeldycke@ocs.online.net>
+#
+# Licensed under the BSD 2-Clause License (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the
+# License at http://opensource.org/licenses/BSD-2-Clause
 
 import platform
 import sys
