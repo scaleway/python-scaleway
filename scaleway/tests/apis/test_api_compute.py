2377930f9d72c4ba2324b5354c8c2e12fc440c38
abarbare <antoinebarbare@gmail.com>
2020-06-04 14:31:36 +0200

    fix: use new instance api url (#32)

--- scaleway/tests/apis/test_api_compute.py
+++ scaleway/tests/apis/test_api_compute.py
@@ -7,15 +7,21 @@ class TestComputeAPI(unittest.TestCase):
 
     def test_set_region(self):
         # Default region: par1.
-        self.assertEqual(ComputeAPI().base_url,
-                         'https://cp-par1.scaleway.com/')
+        self.assertEqual(
+            ComputeAPI().base_url,
+            'https://api-fr-par.scaleway.com/instance/v1/zones/fr-par-1/'
+        )
 
         # Explicit "region".
-        self.assertEqual(ComputeAPI(region='par1').base_url,
-                         'https://cp-par1.scaleway.com/')
+        self.assertEqual(
+            ComputeAPI(region='par1').base_url,
+            'https://api-fr-par.scaleway.com/instance/v1/zones/fr-par-1/'
+        )
 
-        self.assertEqual(ComputeAPI(region='ams1').base_url,
-                         'https://cp-ams1.scaleway.com/')
+        self.assertEqual(
+            ComputeAPI(region='ams1').base_url,
+            'https://api-nl-ams.scaleway.com/instance/v1/zones/nl-ams-1/'
+        )
 
         # Explicit "base_url"
         self.assertEqual(ComputeAPI(base_url='http://whatever').base_url,
