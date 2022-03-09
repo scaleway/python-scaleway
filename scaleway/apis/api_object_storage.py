3424a3b99749db46e3c1d3e944f6db5e8a8227ef
Anthony Martinet <anthony@alkivi.fr>
2022-03-09 14:53:34 +0100

    feat(object-storage): be able to query bucket stats

---
+++ scaleway/apis/api_object_storage.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2013-2016 Online SAS and Contributors. All Rights Reserved.
+#                         Julien Castets <jcastets@scaleway.com>
+#                         Kevin Deldycke <kdeldycke@scaleway.com>
+#
+# Licensed under the BSD 2-Clause License (the "License"); you may not use this
+# file except in compliance with the License. You may obtain a copy of the
+# License at https://opensource.org/licenses/BSD-2-Clause
+
+from . import API
+
+REGIONS = {
+    'fr-par': {
+        'url': 'https://api-fr-par.scaleway.com/object-private/v1/regions/fr-par/',
+    },
+    'nl-ams': {
+        'url': 'https://api-nl-ams.scaleway.com/object-private/v1/regions/nl-ams/',
+    }
+}
+
+
+class ObjectStorageAPI(API):
+    """ The default region is par1 as it was the first availability zone
+    provided by Scaleway, but it could change in the future.
+    """
+
+    def __init__(self, **kwargs):
+        region = kwargs.pop('region', None)
+        base_url = kwargs.pop('base_url', None)
+
+        assert region is None or base_url is None, \
+            "Specify either region or base_url, not both."
+
+        if base_url is None:
+            region = region or 'fr-par'
+
+            assert region in REGIONS, \
+                "'%s' is not a valid Scaleway region." % region
+
+            base_url = REGIONS.get(region)['url']
+
+        super(ObjectStorageAPI, self).__init__(base_url=base_url, **kwargs)
